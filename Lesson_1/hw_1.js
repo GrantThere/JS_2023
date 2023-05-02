8900edae260e99585d1e1c5d33c1e1d0d5b93987
GrantThere <siris.gan@gmail.com>
2023-05-02 16:00:01 +0300

    Initial commit

---
+++ Lesson_1/hw_1.js
@@ -0,0 +1,40 @@
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let h = -999
+let k = 123
+let l = 3.14
+let m =  2.7
+let n = 16
+let q = true
+let r = false
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(h);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+console.log(q);
+console.log(r);
+
+let firstName = 'Ivan '
+let middleName = 'Rost '
+let lastName = 'Gal'
+let Name = firstName+middleName+lastName
+console.log(Name);
+
+a = 100
+b = '100'
+c = true
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
