ce46eabdf77fbe1bf74b103401f60fb37e470668
GrantThere <siris.gan@gmail.com>
2023-06-16 17:39:00 +0300

    Mini Project Fin

--- Project/user-details.js
+++ Project/user-details.js
@@ -1,5 +1,3 @@
-import {requestData,creatDiv} from './request.js';
-
 let uId = localStorage.getItem('userId');
 
 let urla = `https://jsonplaceholder.typicode.com/users/${uId}`;
@@ -7,43 +5,43 @@ let user = requestData(urla);
 
 user.then(user => {
         let mainDiv = document.getElementById('mainDiv');
-        let userInfo = creatDiv();
+        let userInfo = document.createElement('div');
         userInfo.classList.add('userInformation');
         mainDiv.appendChild(userInfo);
-        let uI= creatDiv();
+        let uI= document.createElement('div');
         uI.classList.add('block');
-        let cI= creatDiv();
+        let cI= document.createElement('div');
         cI.classList.add('block');
-        let lI= creatDiv();
+        let lI= document.createElement('div');
         lI.classList.add('block');
         userInfo.append(uI,cI,lI);
-        let inu= creatDiv();
+        let inu= document.createElement('div');
         inu.innerText = `${user.id}.${user.name}||${user.username}`;
         inu.classList.add('Names');
-        let pw= creatDiv();
+        let pw= document.createElement('div');
         pw.innerText = `Phone:${user.phone}     Website:${user.website}`;
         pw.classList.add('phoneWb');
-        let em= creatDiv();
+        let em= document.createElement('div');
         em.innerText = `Email:${user.email}`;
         em.classList.add('email');
         uI.append(inu,pw,em);
-        let cna= creatDiv();
+        let cna= document.createElement('div');
         cna.innerText = `Company:${user.company.name}`;
         cna.classList.add('company');
-        let cP= creatDiv();
+        let cP= document.createElement('div');
         cP.innerText = `${user.company.catchPhrase}`;
         cP.classList.add('phoneWb');
-        let bs= creatDiv();
+        let bs= document.createElement('div');
         bs.innerText = `${user.company.bs}`;
         bs.classList.add('email');
         cI.append(cna,cP,bs);
-        let adr = creatDiv();
+        let adr = document.createElement('div');
         adr.innerText = `Address: ${user.address.suite},${user.address.street},${user.address.city}`;
         adr.classList.add('adress');
-        let zip = creatDiv();
+        let zip = document.createElement('div');
         zip.classList.add('phoneWb');
         zip.innerText = `Zipcode: ${user.address.zipcode}`;
-        let geo = creatDiv();
+        let geo = document.createElement('div');
         geo.classList.add('email');
         geo.innerText = `${user.address.geo.lat}x${user.address.geo.lng}`;
         lI.append(adr,zip,geo);
@@ -58,7 +56,7 @@ let posts = requestData(postUrl);
 
 posts.then(post =>{
         setTimeout(() => {let mainDiv = document.getElementById('mainDiv');
-                let postssssss = creatDiv();
+                let postssssss = document.createElement('div');
                 postssssss.classList.add('userPosts');
                 mainDiv.appendChild(postssssss);
                 let pBtn = document.createElement('button');
@@ -71,10 +69,10 @@ posts.then(post =>{
                         for (const po of post) {
                                 let postTitle = document.createElement('li');
                                 postTitleList.appendChild(postTitle);
-                                let hPost = creatDiv();
+                                let hPost = document.createElement('div');
                                 hPost.classList.add('title');
                                 hPost.innerText = `${po.title}`;
-                                let postBut = creatDiv();
+                                let postBut = document.createElement('div');
                                 postBut.classList.add('postBut');
                                 postTitle.append(hPost,postBut);
                                 let postUrla = document.createElement('a');
@@ -85,3 +83,9 @@ posts.then(post =>{
                         }
                 };}, 50);
 })
+
+async function requestData(url) {
+        let response = await fetch(url)
+            .then(response => response.json());
+        return response;
+}
